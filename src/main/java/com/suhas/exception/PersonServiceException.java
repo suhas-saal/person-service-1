10ff0dc609263644dffd82e98b8281c5796fde6a
Suhas Saheer <suhaz786@gmail.com>
2020-01-19 22:30:49 +0400

    Custom Exception

--- src/main/java/com/suhas/exception/PersonServiceException.java
+++ src/main/java/com/suhas/exception/PersonServiceException.java
@@ -1,7 +1,7 @@
 package com.suhas.exception;
 
 /**
- * Custom @{@link RuntimeException} for the AuthenticationService
+ * Custom @{@link RuntimeException} for the @{@link com.suhas.PersonServiceApplication}
  */
 public class PersonServiceException extends RuntimeException {
 
